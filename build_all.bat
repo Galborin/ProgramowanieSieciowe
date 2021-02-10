2e58b33f566a31ea488836beeee4b9d1d5197ebd
Piotr Koziar <piotrkoziar@hotmail.com>
2021-02-10 01:43:02 +0100

    Improved client

--- build_all.bat
+++ build_all.bat
@@ -1,2 +1,2 @@
 g++ -o server .\src\*.cpp -lws2_32 -losapi -I.\src\ -I.\include\ -L.
-g++ -o app_client .\client\*.cpp -lws2_32
+g++ -o app_client .\client\app_client.cpp -lws2_32 -losapi -I.\include\ -L. 
